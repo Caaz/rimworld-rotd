abbd205627fbe0063e285228f1e8ff94135f32ee
Daniel Caaz <dcaazy@gmail.com>
2022-05-04 20:30:35 -0700

    Some proof of concept generation

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -1,12 +1,43 @@
 using HugsLib;
+using Verse;
 
-namespace MyMod
+namespace RimOfTheDead
 {
     public class Mod : ModBase
     {
         public override void DefsLoaded()
         {
-            Logger.Message("Hello, World!");
+            GenerateDefs();
+        }
+        private void GenerateDefs()
+        {
+            foreach (PawnKindDef pawnKindDef in DefDatabase<PawnKindDef>.AllDefsListForReading)
+            {
+                if (!CanBeUndead(pawnKindDef)) continue;
+                GeneratePawnKindDef(pawnKindDef);
+                GenerateThingDef(pawnKindDef.race);
+            }
+        }
+        private PawnKindDef GeneratePawnKindDef(PawnKindDef sourceKind)
+        {
+            Logger.Message("Generating PawnKind for " + sourceKind.label);
+            PawnKindDef newKind = new PawnKindDef();
+            newKind.label = sourceKind.label;
+            newKind.defName = "ROTD_" + sourceKind.defName;
+            return newKind;
+        }
+        private ThingDef GenerateThingDef(ThingDef sourceThing)
+        {
+            Logger.Message("Generating ThingDef for " + sourceThing.label);
+            ThingDef newThing = new ThingDef();
+            return newThing;
+        }
+        private bool CanBeUndead(PawnKindDef pawnKindDef)
+        {
+            return (
+                (pawnKindDef.RaceProps.IsFlesh)
+                && (!pawnKindDef.label.ToLower().Contains("zombie"))
+            );
         }
     }
 }
