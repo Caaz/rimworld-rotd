e7078198eab1d4dfa8d7d33bd9ae024a6ce5b9b6
Daniel Caaz <dcaazy@gmail.com>
2022-05-05 23:19:41 -0700

    New generator

--- Source/Mod.cs
+++ Source/Mod.cs
@@ -1,5 +1,8 @@
 using HugsLib;
+using HugsLib.Utils;
 using Verse;
+using RimWorld;
+using UnityEngine;
 
 namespace RimOfTheDead
 {
@@ -11,25 +14,48 @@ namespace RimOfTheDead
         }
         private void GenerateDefs()
         {
-            foreach (PawnKindDef pawnKindDef in DefDatabase<PawnKindDef>.AllDefsListForReading)
+            List<PawnKindDef> pawnkinds = DefDatabase<PawnKindDef>.AllDefsListForReading;
+            foreach (PawnKindDef pawnKindDef in pawnkinds)
             {
                 if (!CanBeUndead(pawnKindDef)) continue;
-                GeneratePawnKindDef(pawnKindDef);
-                GenerateThingDef(pawnKindDef.race);
+                ThingDef zombieDef = GenerateThingDef(pawnKindDef.race);
+                GeneratePawnKindDef(pawnKindDef, zombieDef);
             }
         }
-        private PawnKindDef GeneratePawnKindDef(PawnKindDef sourceKind)
+        private PawnKindDef GeneratePawnKindDef(PawnKindDef sourceKind, ThingDef zombieDef)
         {
             Logger.Message("Generating PawnKind for " + sourceKind.label);
             PawnKindDef newKind = new PawnKindDef();
-            newKind.label = sourceKind.label;
+
+            newKind.label = "Zombie " + sourceKind.label;
             newKind.defName = "ROTD_" + sourceKind.defName;
+            newKind.race = zombieDef;
+
+            InjectedDefHasher.GiveShortHashToDef(newKind, typeof(PawnKindDef));
+            DefDatabase<PawnKindDef>.Add(newKind);
             return newKind;
         }
         private ThingDef GenerateThingDef(ThingDef sourceThing)
         {
+            string defName = "ROTD_Zombie_" + sourceThing.defName;
+            ThingDef newThing = DefDatabase<ThingDef>.GetNamedSilentFail(defName);
+            if (newThing != null) return newThing;
+
             Logger.Message("Generating ThingDef for " + sourceThing.label);
-            ThingDef newThing = new ThingDef();
+            newThing = new ThingDef();
+
+            newThing.thingClass = sourceThing.thingClass;
+            newThing.category = sourceThing.category;
+
+            if (sourceThing.graphicData != null)
+            {
+                newThing.graphicData = new GraphicData();
+                newThing.graphicData.CopyFrom(sourceThing.graphicData);
+                newThing.graphicData.color = new Color(1f, 0f, 0f, 1f);
+            }
+
+            InjectedDefHasher.GiveShortHashToDef(newThing, typeof(ThingDef));
+            DefDatabase<ThingDef>.Add(newThing);
             return newThing;
         }
         private bool CanBeUndead(PawnKindDef pawnKindDef)
